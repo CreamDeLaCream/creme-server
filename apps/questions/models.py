6e5a170333b9b5f8203095f630f506adf77c015e
rigyeonghong <zxcvbee@naver.com>
2022-02-26 00:55:55 +0900

    feat: Implement question model
    Co-authored-by: yeongkwang <2ykwang@users.noreply.github.com>

--- apps/questions/models.py
+++ apps/questions/models.py
@@ -1,3 +1,22 @@
-# from django.db import models
+from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class Question(models.Model):
+
+    question = models.TextField(verbose_name=_("question"))
+
+    class Meta:
+        verbose_name = _("question")
+        db_table = "question"
+
+
+class QuestionChoice(models.Model):
+
+    choice_index = models.IntegerField(verbose_name=_("index"))
+    content = models.TextField(verbose_name=_("question choice content"))
+    solution = models.TextField(verbose_name=_("question choice solution"))
+
+    class Meta:
+        verbose_name = _("question choice")
+        db_table = "question_choice"
