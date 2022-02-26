ca555dcfddf20a2bc3cf376f66a44fd396944568
rigyeonghong <zxcvbee@naver.com>
2022-02-26 16:58:50 +0900

    feat : Add url.py for each app to seperate url

--- config/urls.py
+++ config/urls.py
@@ -1,10 +1,14 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("users/", include("apps.users.urls")),
+    path("analysis/", include("apps.analysis.urls")),
+    path("dogs/", include("apps.dogs.urls")),
+    path("questions/", include("apps.questions.urls")),
 ]
 
 # DEBUG일 떄 WAS에서 서빙
