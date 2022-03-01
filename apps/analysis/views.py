ac3c254a38efe3a9669b78511078f834aef87347
rigyeonghong <77164776+rigyeonghong@users.noreply.github.com>
2022-03-02 05:42:39 +0900

    Add git submodule creme-ai (#11)
    * feat : Use git sub module to serve the ai model
    
    * feat : Use git sub module to serve the ai model
    
    * feat : Create soft link to expose that file path from submodule
    
    * chore : Add package to run the ai model
    
    * Update analysis view.py to run ai model
    
    * feat: Change sub-module path
    
    * feat: Change submodule path
    
    Co-authored-by: Yeonggwang <immutable000@gmail.com>

--- apps/analysis/views.py
+++ apps/analysis/views.py
@@ -1,3 +1,10 @@
+
+import cv2
+from django.http import HttpResponse
+
+from creme_ai.dogemotion import dog_model
+from creme_ai.humanemotion import human_model
+
 from rest_framework import parsers
 from rest_framework.generics import CreateAPIView
 
@@ -15,3 +22,23 @@ class AnalysisPetView(CreateAPIView):
     serializer_class = AnalysisPetSerializer
     queryset = Analysis.objects.all()
     parser_classes = (parsers.MultiPartParser,)
+
+def analysis_model(request):
+    image_path = "sample image path"
+    landmark_detector_path = "landmark detector path"
+    dog_head_detector_path = "dog head detector path"
+    model_path = "model path"
+
+    model = dog_model.DogModel(
+        landmark_detector_path, dog_head_detector_path, model_path
+    )
+
+    dog_result = model.predict(image_path)
+
+    human_image_path = "humanemotion/image.png"
+    img = cv2.imread(human_image_path)
+    human_result = human_model.RMN().detect_emotion_for_single_frame(img)
+
+    result = dog_result, human_result
+
+    return HttpResponse(result)
