f28295de60c3316f87bfd7aa1af4f6a5979644c5
Yeonggwang <immutable000@gmail.com>
2022-03-01 21:05:08 +0900

    Update CORS_ALLOWED_ORIGINS in local

--- config/settings/local.py
+++ config/settings/local.py
@@ -3,4 +3,9 @@ from .base import *  # noqa: F401,F403
 ALLOWED_HOSTS = ["*"]
 CORS_ORIGIN_ALLOW_ALL = True
 
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3001",
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+]
 DEBUG = True
