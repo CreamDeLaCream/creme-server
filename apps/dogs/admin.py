545416147ebdeb686a8d3b265902a2785c3e73fe
rigyeonghong <zxcvbee@naver.com>
2022-03-02 19:03:25 +0900

    feat: Add dogkeyword data with json file

--- apps/dogs/admin.py
+++ apps/dogs/admin.py
@@ -1,6 +1,19 @@
 from django.contrib import admin
 
-# Register your models here.
-from .models import Dog
+from .models import Dog, DogKeyword
 
-admin.site.register(Dog)
+
+@admin.register(Dog)
+class DogAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "name",
+    )
+
+
+@admin.register(DogKeyword)
+class DogKeywordAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "name",
+    )
