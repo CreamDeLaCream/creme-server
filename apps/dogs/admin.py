6f737706f15739e0484177dddb0f0956a21010a7
rigyeonghong <zxcvbee@naver.com>
2022-03-04 05:48:38 +0900

    feat : Implement DogCharacter model to match the dog keyword

--- apps/dogs/admin.py
+++ apps/dogs/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Dog, DogKeyword
+from .models import Dog, DogCharacter, DogKeyword
 
 
 @admin.register(Dog)
@@ -9,6 +9,12 @@ class DogAdmin(admin.ModelAdmin):
         "id",
         "name",
     )
+
+    # manytomany field 추가 삭제
+    filter_horizontal = ("dog_keyword",)
+
+
+admin.site.register(DogCharacter)
 
 
 @admin.register(DogKeyword)
