9d64ca92ab851029001c25ce6fa3c0253f38378e
Yeonggwang <immutable000@gmail.com>
2022-03-03 17:31:02 +0900

    Load AI model

--- apps/analysis/apps.py
+++ apps/analysis/apps.py
@@ -1,6 +1,16 @@
 from django.apps import AppConfig
 
+from creme_ai.dogemotion.dog_model import DogModel
+from creme_ai.humanemotion.human_model import RMN
+
 
 class AnalysisConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "apps.analysis"
+
+    human_ai_model = RMN()
+    dog_ai_model = DogModel(
+        "creme_ai/dogemotion/landmarkDetector.dat",
+        "creme_ai/dogemotion/dogHeadDetector.dat",
+        "creme_ai/dogemotion/classifierRotatedOn100Ratio90Epochs100.h5",
+    )
