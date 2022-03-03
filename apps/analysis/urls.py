8b8d4038c0de5a024cbac7ce35cbd48b5a3ff136
yeongkwang <immutable000@gmail.com>
2022-03-04 05:52:11 +0900

    Integrate ai model with Django (#12)
    * Load AI model
    
    * Fix cors error
    
    * Update field type
    
    * Fix lock file version
    
    * Feat: analysis dog emotion
    
    * Update questionchoice fixture data
    
    * Update fields name
    
    * Add human emotion serializer
    
    * Rename fixtures data
    
    * Add django command to load data
    
    python manage.py loaddata
    
    * Add `Need` model

--- apps/analysis/urls.py
+++ apps/analysis/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import AnalysisPetView
+from .views import AnalysisHumanView, AnalysisPetView
 
 urlpatterns = [
     path("pet", AnalysisPetView.as_view()),
+    path("human", AnalysisHumanView.as_view()),
 ]
--- config/settings/local.py
+++ config/settings/local.py
@@ -1,7 +1,7 @@
 from .base import *  # noqa: F401,F403
 
 ALLOWED_HOSTS = ["*"]
-CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOW_ALL_ORIGINS = True
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3001",
