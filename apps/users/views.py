695211d3a8d7061d9f2fa464a299c48992415f06
rigyeonghong <zxcvbee@naver.com>
2022-03-07 02:26:16 +0900

    fix : Fix TokenAPIView to get kakao access token

--- apps/users/views.py
+++ apps/users/views.py
@@ -1,4 +1,5 @@
 import requests
+from django.conf import settings
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework_simplejwt.tokens import RefreshToken
@@ -16,27 +17,35 @@ class TokenAPIView(APIView):
     Signup or Signin class
     """
 
-    def post(self, request):
-        token_json = request.json()
+    def get(self, request):
+        # code를 통해 kakao access token 발급
+
+        code = request.GET.get("code")
+        client_id = getattr(settings, "KAKAO_REST_API_KEY")
+        redirect_uri = "http://localhost:3000/users/kakao/callback"
+        kakao_oauthurl = f"https://kauth.kakao.com/oauth/token?grant_type=authorization_code&client_id={client_id}&redirect_uri={redirect_uri}&code={code}"  # noqa = E501
+
+        # 인증코드를 토큰서버로 보내야함
+        token_request = requests.get(kakao_oauthurl)
+        # 보낸 요청에 대한 데이터(토큰)를 받음
+        token_json = token_request.json()
 
         if "error" in token_json:
             return Response("에러가 발생했습니다!")
 
         access_token = token_json["access_token"]
 
+        # 발급 받은 access_token을 통해 유저 이메일을 가져옴
         profile_request = requests.get(
             "https://kapi.kakao.com/v2/user/me",
             headers={"Authorization": f"Bearer {access_token}"},
         )
 
         data = profile_request.json()
-
         kakao_account = data["kakao_account"]
         email = kakao_account["email"]
         username = kakao_account["profile"]["nickname"]
 
-        email = request.email
-        username = request.username
         try:
             user = User.objects.get(email=email)
             User.objects.filter(email=email).update(email=email, username=username)
