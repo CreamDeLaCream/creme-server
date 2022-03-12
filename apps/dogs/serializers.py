b1210d62589552f6c9660ca15d3fe14b22da4f7c
rigyeonghong <zxcvbee@naver.com>
2022-03-12 20:05:47 +0900

    fix : Fix dog list api bug

--- apps/dogs/serializers.py
+++ apps/dogs/serializers.py
@@ -1,6 +1,3 @@
-from typing import List
-
-from django.db.models import QuerySet
 from rest_framework import serializers
 
 from .models import Dog, DogKeyword
@@ -11,23 +8,12 @@ class DogSerializer(serializers.ModelSerializer):
 
     dog_keyword = serializers.ListSerializer(
         child=serializers.CharField(max_length=20),
+        required=False,
     )
-
-    def _make_keywords_from_list(self, keyword_list: List[str]) -> QuerySet:
-
-        qs = DogKeyword.objects.filter(name__in=keyword_list)
-        return qs
-
-    def validate_dog_keyword(self, value):
-        keywords = self._make_keywords_from_list(value)
-        return keywords
 
     def create(self, validated_data):
         # list 를 가져옴
-        keywords = validated_data.pop("dog_keyword")
-
         dog = Dog.objects.create(**validated_data)
-        dog.dog_keyword.set(keywords)
         return dog
 
     class Meta:
@@ -39,7 +25,7 @@ class DogSerializer(serializers.ModelSerializer):
             "age",
             "dog_keyword",
             "image",
-            # "user",
+            "user",
         )
         # https://www.django-rest-framework.org/api-guide/fields/#core-arguments 참고
         extra_kwargs = {
